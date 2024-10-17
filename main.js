db9db3687ee78a5a5663e9d4a8a4ce781a984563
Stephan Grein <stephan.grein@uni-bonn.de>
2024-10-17 11:39:52 +0200

    Pulling Shiny app CALOR for architecture linux/amd64.

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ function checkDockerInstallation() {
 // Function to run the Docker container using elevated privileges
 function dockerRunWithPrivileges(imageName, containerName, hostPort, containerPort) {
   return new Promise((resolve, reject) => {
-    const command = `docker run -d -p ${hostPort}:${containerPort} --name ${containerName} ${imageName}`;
+    const command = `docker run -d -p ${hostPort}:${containerPort} --platform linux/amd64 --name ${containerName} ${imageName}`;
     const options = {
       name: 'MyElectronApp',
     };
@@ -122,7 +122,7 @@ function dockerRunWithPrivileges(imageName, containerName, hostPort, containerPo
 
 function dockerPullWithPrivileges(imageName) {
   return new Promise((resolve, reject) => {
-    const command = `docker pull ${imageName}`;
+    const command = `docker pull --platform linux/amd64 ${imageName}`;
     const options = {
       name: 'MyElectronApp',  // This name appears in the prompt dialog
     };
